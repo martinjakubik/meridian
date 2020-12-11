122d7dd8c9663f2222a81ff2ed218a24d2c5e939
martin jakubik <martin.jakubik@gmail.com>
2020-12-11 16:12:10 +0100

    converted grid to es6 object

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,12 @@
 import { createCanvas, getStyles } from '../../lib/js/hypertext.js';
 import { fillSquare, redrawGrid } from './drawing.js';
-import { makeGrid } from './grid.js';
+import { Grid } from './grid.js';
 
 const CANVAS_ID = 'meridian-map';
 const GRID_WIDTH = 16;
 const GRID_CELL_SIZE = 30;
 
-let oGrid = makeGrid(GRID_WIDTH);
+let oGrid = new Grid(GRID_WIDTH);
 
 let oMeridianCanvas = createCanvas(CANVAS_ID, 'grid', 0);
 redrawGrid(oMeridianCanvas, GRID_WIDTH, GRID_CELL_SIZE);
@@ -33,10 +33,10 @@ let handleMeridianCanvasTapped = function (event) {
     let x = event.clientX;
     let y = event.clientY;
     let oClosestCell = getClosestCell(oCanvasOffset, GRID_CELL_SIZE, x, y);
-    let nClosestCellX = oClosestCell.x;
-    let nClosestCellY = oClosestCell.y;
-    fillSquare(oMeridianCanvas, nClosestCellX, nClosestCellY, GRID_CELL_SIZE, true);
-    oGrid[nClosestCellX][nClosestCellY] = 1;
+    let nCellX = oClosestCell.x;
+    let nCellY = oClosestCell.y;
+    fillSquare(oMeridianCanvas, nCellX, nCellY, GRID_CELL_SIZE, true);
+    oGrid.toggleCell(nCellX, nCellY);
 
 };
 oMeridianCanvas.onclick = handleMeridianCanvasTapped;
--- app/js/grid.js
+++ app/js/grid.js
@@ -1,16 +1,26 @@
-/**
- * initilazes a grid containing all zeros
- * @param {Number} nGridWidth the width of the grid in cells
- */
-let makeGrid = function(nGridWidth) {
-    let oGrid = [];
-    for (let y = 0; y < nGridWidth; y++) {
-        oGrid.push([]);
-        for (let x = 0; x < nGridWidth; x++) {
-            oGrid[y].push(0);
-        }
-    }
-    return oGrid;
-};
+class Grid {
 
-export { makeGrid };
+    /**
+     * initilazes a grid containing all zeros
+     * 
+     * @param {Number} gridWidth the width of the grid in cells
+     */
+    constructor(gridWidth) {
+
+        this.gridModel = [];
+        for (let y = 0; y < gridWidth; y++) {
+            this.gridModel.push([]);
+            for (let x = 0; x < gridWidth; x++) {
+                this.gridModel[y].push(0);
+            }
+        }
+
+    }
+
+    toggleCell(x, y) {
+        this.gridModel[y][x] = this.gridModel[y][x] === 0 ? 1 : 0;
+    }
+
+}
+
+export { Grid };
